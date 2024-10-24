92381471d8be2cac2342f3535357a50c932cfbcc
Iago Ferreira <iago.ferreira@ops.team>
2024-10-24 20:51:21 -0300

    add site

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region = "us-east-1"  # Defina sua região
+  region = "us-east-1"  # Defina sua região ou use 'sa-east-1' para São Paulo
 }
 
+# Configuração do backend para armazenar o estado no S3
 terraform {
   backend "s3" {
     bucket = "tfstate-formacao-devops"  # Nome do bucket para armazenar o tfstate
@@ -10,15 +11,29 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "formacao-devops-pipeline"  # Nome do bucket S3
+# Bucket S3 que servirá como website
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "meu-bucket-exemplo-website"  # Nome único do bucket S3
+  acl    = "public-read"                 # Permissões públicas para acesso ao website
+
+  website {
+    index_document = "index.html"        # Define o arquivo de índice principal
+  }
 
   tags = {
-    Name        = "formacao-devops-pipeline"
+    Name        = "MeuBucketWebsite"
     Environment = "Development"
   }
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.my_bucket.bucket
+# Arquivo HTML que será hospedado no bucket
+resource "aws_s3_bucket_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket  # Refere-se ao bucket criado
+  key    = "index.html"
+  source = "index.html"                         # Fonte local do arquivo HTML
+  acl    = "public-read"                        # Permissões públicas para o arquivo
+}
+
+output "bucket_website_url" {
+  value = aws_s3_bucket.website_bucket.website_endpoint  # URL do bucket para acessar o site
 }
