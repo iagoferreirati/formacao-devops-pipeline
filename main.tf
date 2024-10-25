9e55f597f774b033c0e7fc0d93f799b4bec1de3e
Iago Ferreira <iago.ferreira@ops.team>
2024-10-24 21:00:46 -0300

    add acl

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ terraform {
 # Bucket S3 que servirá como website
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "formacao-devops-pipeline"  # Nome único do bucket S3
+
   website {
     index_document = "index.html"        # Define o arquivo de índice principal
   }
@@ -29,13 +30,17 @@ resource "aws_s3_bucket_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket  # Refere-se ao bucket criado
   key    = "index.html"
   source = "index.html"                         # Fonte local do arquivo HTML
-  }
-
-output "bucket_website_url" {
-  value = aws_s3_bucket.website_bucket.website_endpoint  # URL do bucket para acessar o site
+  content_type = "text/html"                    # Define o tipo de conteúdo como HTML
+  acl    = "public-read"                        # Garante que o arquivo HTML seja acessível publicamente
 }
 
-resource "aws_s3_bucket_acl" "example" {
+# Configura a ACL do bucket para ser publicamente acessível
+resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.website_bucket.id
   acl    = "public-read"
+}
+
+# Output da URL do bucket website
+output "bucket_website_url" {
+  value = aws_s3_bucket.website_bucket.website_endpoint  # URL do bucket para acessar o site
 }
